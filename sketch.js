afa24daf52866fbe53c64d7f666f86eafcc47c8c
Arhaant <76239002+Arhaant@users.noreply.github.com>
2021-02-27 19:05:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var ball, database;
+var position;
+
+var form;
+
+var gameState = 0
+
+var control
+
+var player
+
+var playerCount;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+
+
+    control = new Control()
+    control.gameStart()
+}
+
+function draw(){
+    background("white");
+
+
+ 
+
+
+
+
+
+    
+}
+
+
+
+
+
